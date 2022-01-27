1279cc20054ad13d0483f8e054202d796c19d391
HLuna <christopher.luna.2014@gmail.com>
2022-01-27 16:08:24 -0500

    V12 Hallando descuento en HTML desde Js

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,17 @@
-console.log("gaa");
 
 function calcularDescuento(monto, descuento){
     const nuevoMonto = (descuento * monto)/100;
     return nuevoMonto;
+}
+
+function calcularDescuentosTotales(){
+    const monto = document.getElementById("inputMonto");
+    const descuento = document.getElementById("inputDescuento");
+
+    const montoValue = monto.value;
+    const descuentoValue = descuento.value;
+    const msm = calcularDescuento(montoValue, descuentoValue);
+    const descuentoDecimal = document.getElementById("parrafo");
+    descuentoDecimal.innerText = `El descuento de ${descuentoValue}% equivale a ${msm}`;
+
 }
