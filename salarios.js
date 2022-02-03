da219ee49858d7fdffc300fddbd30c0ac91c428a
HLuna <christopher.luna.2014@gmail.com>
2022-02-03 11:41:21 -0500

    V19,20,21,22,23 salarios html,js analisis js

---
+++ salarios.js
@@ -0,0 +1,39 @@
+const colombia = [];
+
+colombia.push({
+    name: "Camila",
+    salary: 500,
+});
+colombia.push({
+    name: "Camila",
+    salary: 500,
+});
+colombia.push({
+    name: "Camila",
+    salary: 3500,
+});
+colombia.push({
+    name: "Camila",
+    salary: 1500,
+});
+colombia.push({
+    name: "Camila",
+    salary: 2500,
+});
+colombia.push({
+    name: "Camila",
+    salary: 1500,
+});
+colombia.push({
+    name: "Camila",
+    salary: 2500,
+});
+colombia.push({
+    name: "Camila",
+    salary: 1500,
+});
+
+colombia.push({
+    name: "Camila",
+    salary: 2500,
+});
